01bcc918beee8264245d0977ac57b4ec61396b99
vlerabexheti <118972559+vlerabexheti@users.noreply.github.com>
2023-02-03 20:50:15 +0100

    Delete GymManagmentSystem Java/maven directory

--- GymManagmentSystem Java/maven/src/main/java/com/example/maven/pojo/Member/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.maven.pojo.Member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-}
